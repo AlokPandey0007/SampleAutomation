bb3847e26ec691eec1dec0223295356451ab6475
Alok Pandey <alok.pandey02@nagarro.com>
2025-04-06 15:17:16 +0530

    first commit

---
+++ src/test/java/SampleProject/Automation/AppTest.java
@@ -0,0 +1,23 @@
+package SampleProject.Automation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void Test1() {
+    assertTrue(5==5);
+  }
+  
+  @Test
+  public void Test2() {
+    assertTrue('A'=='A');
+  }
+  
+  @Test
+  public void Test3() {
+    assertTrue('5'=='5');
+  }
+}
