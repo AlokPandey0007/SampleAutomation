2d5a17bf213ef40cb3d819e5204d035f64f17522
AlokPandey0007 <alok.pandey02@nagarro.com>
2025-04-07 08:54:50 +0530

    changed to testng

--- src/test/java/SampleProject/Automation/AppTest.java
+++ src/test/java/SampleProject/Automation/AppTest.java
@@ -1,23 +1,42 @@
 package SampleProject.Automation;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class AppTest {
-    
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void beforeMethod()
+	{
+		driver = new ChromeDriver();
+	}
   @Test
   public void Test1() {
-    assertTrue(5==5);
+     
+    driver.get("https://google.com");
+    Assert.assertEquals("Google",driver.getTitle());   
+  
   }
   
   @Test
   public void Test2() {
-    assertTrue('A'=='A');
+	    Assert.assertEquals(true, true);
   }
   
   @Test
   public void Test3() {
-    assertTrue('5'=='5');
+	    Assert.assertEquals(true, true);
+  }
+  
+  @AfterMethod
+  public void tearDown()
+  {
+	driver.quit();  
   }
 }
